eb964fa90807034e9c6d6c548d3f9df5fd0597c8
Anton <anton@ondoc.me>
2017-07-21 15:42:50 +0300

    Create migration CreatePatients

---
+++ db/migrate/20170721123347_create_patients.rb
@@ -0,0 +1,11 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.string :surname
+      t.string :patronymic
+      t.string :phone
+      t.string :email
+    end
+  end
+end
