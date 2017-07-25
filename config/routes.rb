7ccf6f6c37f251a800acc3571636dd2afe9fb428
Anton <anton@ondoc.me>
2017-07-25 15:45:27 +0300

    Create Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :patients, controllers: {
       sessions: 'patients/sessions'
   }
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
 
   # Example of regular route:
