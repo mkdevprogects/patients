145801eef82224e66dedc64e60a6457184a9c18e
Anton <anton@ondoc.me>
2017-07-27 12:26:07 +0300

    Update controllers

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,4 +1,5 @@
 class ClinicsController < ApplicationController
+  before_action :authenticate_patient!
   def index
   end
 end
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,5 @@
 class DoctorsController < ApplicationController
+  before_action :authenticate_patient!
   def index
     @doctors = Doctor.all
   end
