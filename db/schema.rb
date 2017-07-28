c60f399ef74e73b1fb32f2ea07df9a10de8f0fdf
Anton <anton@ondoc.me>
2017-07-28 20:17:34 +0300

    Add column code to Specialization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728105048) do
+ActiveRecord::Schema.define(version: 20170728171505) do
 
   create_table "clinics", force: :cascade do |t|
     t.string   "title"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20170728105048) do
 
   create_table "specializations", force: :cascade do |t|
     t.string "title"
+    t.string "code"
   end
 
 end
