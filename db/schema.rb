1f83886abd259ae75e156c36330942b5981b538e
Anton <anton@ondoc.me>
2017-07-28 21:57:27 +0300

    Crate model Grade

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728171505) do
+ActiveRecord::Schema.define(version: 20170728185327) do
 
   create_table "clinics", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 20170728171505) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "grades", force: :cascade do |t|
+    t.string "title"
   end
 
   create_table "illnesses", force: :cascade do |t|
