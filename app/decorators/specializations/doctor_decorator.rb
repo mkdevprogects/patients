d569d14b2be0c526088d9186bc31fd322e5e31bc
Anton <anton@ondoc.me>
2017-07-30 23:54:01 +0300

    Update specializatios/id/doctors view and decorator

--- app/decorators/specializations/doctor_decorator.rb
+++ app/decorators/specializations/doctor_decorator.rb
@@ -1,29 +1,6 @@
 module Specializations
-  class DoctorDecorator < Draper::Decorator
+  class DoctorDecorator < ::DoctorDecorator
     delegate_all
 
-    def specializations_title
-      object.specializations.map {|s| s.title}.join(', ')
-    end
-
-    def grade
-      if object.grades.nil?
-        ' Врач'
-      else
-        object.grades.first.title
-      end
-    end
-
-    def grades
-      if object.grades.nil?
-        ' Врач'
-      else
-        object.grades.map {|s| s.title}.join(', ')
-      end
-    end
-
-    def clinics
-      object.clinics.map {|c| c.title}.join(', ')
-    end
   end
 end
