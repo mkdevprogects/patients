6fa03deeadf8058e6af65ed8c2baec72728bfd7b
Anton <anton@ondoc.me>
2017-07-30 19:06:23 +0300

    Create filter on ClinicsController

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,15 +1,25 @@
 class ClinicsController < BaseController
+  before_action :set_clinic, :set_specializations_title, only: [:show]
+
   def index
     @clinics = Clinic.all
   end
 
   def show
+  end
+
+  private
+
+  def set_clinic
     @clinic = Clinic.find(params[:id])
-    @specializations = []
+  end
+
+  def set_specializations_title
+    @specializations_title = []
     @clinic.doctors.each  do |doctor|
-        doctor.specializations.each do |s|
-          @specializations << s.title if @specializations.include?(s.title) == false
-        end
+      doctor.specializations.each do |s|
+        @specializations_title << s.title if @specializations_title.include?(s.title) == false
+      end
     end
   end
 end
