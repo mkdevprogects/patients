6651aed607472cd68e08d31269e740c3a4d98fb4
Anton <anton@ondoc.me>
2017-08-06 18:35:48 +0300

    Fix bugs

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -7,6 +7,12 @@ class Clinic < ActiveRecord::Base
   validates :title, presence: true
   validates :email, presence: true, uniqueness: true
 
+  # scope :specializations_titles, -> { (joins(:doctos)) }
+
+  # def specializations_titles
+  #   Specialization.joins(:doctors).joins(:clinics).where("clinics.id=?", self.id)
+  # end
+
   def specializations_titles
     doctors.reduce([]) do |result, doctor|
       result << doctor.specializations.pluck(:title)
