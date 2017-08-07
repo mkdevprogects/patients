856359d51542aa2471ed1eab4e024832337cc5b9
Anton <anton@ondoc.me>
2017-08-07 23:58:41 +0300

    Refactor Disease.clinics

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -12,17 +12,6 @@ class Disease < ActiveRecord::Base
   validates :icd_code, uniqueness: true
 
   def clinics
-    specializations.reduce([]) do |result, spec|
-      result << spec_clinics(spec)
-    end.flatten.uniq
-  end
-
-
-  private
-
-  def spec_clinics(spec)
-    spec.doctors.reduce([]) do |result, doctor|
-      result << doctor.clinics
-    end
+    Clinic.joins(doctors: [specializations: [:diseases]]).where("diseases.id=?", self.id).uniq
   end
 end
