a5236db41307896451bb5d19ee9a50742a836847
Anton <anton@ondoc.me>
2017-08-08 00:12:08 +0300

    Create validation title and code to Specialization

--- spec/models/specialization_spec.rb
+++ spec/models/specialization_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Specialization, type: :model do
   it { is_expected.to have_many(:doctors) }
   it { is_expected.to have_many(:diseases) }
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:code) }
 end
