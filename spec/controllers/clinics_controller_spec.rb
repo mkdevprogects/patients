f72464f1c8876d0204018acc7bc46ac8166d40ac
Anton <anton@ondoc.me>
2017-08-08 19:04:43 +0300

    Create several tests from ClinicsController

--- spec/controllers/clinics_controller_spec.rb
+++ spec/controllers/clinics_controller_spec.rb
@@ -9,6 +9,21 @@ RSpec.describe ClinicsController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the index template" do
+      sign_in patient
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    let(:clinic_1) { create(:clinic) }
+    let(:clinic_2) { create(:clinic) }
+
+    it "loads all of the clinics into @clinics" do
+      sign_in patient
+      get :index
+      expect(assigns(:clinics)).to match_array([clinic_1, clinic_2])
+    end
   end
 
   describe "GET #show" do
