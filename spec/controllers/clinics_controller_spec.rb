3591987dc28c4c39c401d21469065d8510086c3e
Anton <anton@ondoc.me>
2017-08-08 20:23:51 +0300

    Write tests to DiseasesController

--- spec/controllers/clinics_controller_spec.rb
+++ spec/controllers/clinics_controller_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe ClinicsController, type: :controller do
   let(:patient) { create(:patient) }
 
   describe "GET #index" do
+    let(:clinic_1) { create(:clinic) }
+    let(:clinic_2) { create(:clinic) }
+
     it "returns http success" do
       sign_in patient
       get :index
@@ -16,10 +19,7 @@ RSpec.describe ClinicsController, type: :controller do
       expect(response).to render_template("index")
     end
 
-    let(:clinic_1) { create(:clinic) }
-    let(:clinic_2) { create(:clinic) }
-
-    it "loads all of the clinics into @clinics" do
+    it "loads all clinics" do
       sign_in patient
       get :index
       expect(assigns(:clinics)).to match_array([clinic_1, clinic_2])
@@ -33,6 +33,12 @@ RSpec.describe ClinicsController, type: :controller do
       sign_in patient
       get :show, { id: clinic.id}
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      sign_in patient
+      get :show, { id: clinic.id}
+      expect(response).to render_template("show")
     end
   end
 end
--- spec/controllers/diseases_controller_spec.rb
+++ spec/controllers/diseases_controller_spec.rb
@@ -4,10 +4,25 @@ RSpec.describe DiseasesController, type: :controller do
   let(:patient) { create(:patient) }
 
   describe "GET #index" do
+    let(:disease_1) { create(:disease, icd_code: 'A00-A00') }
+    let(:disease_2) { create(:disease, icd_code: 'B01-B01') }
+
     it "returns http success" do
       sign_in patient
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      sign_in patient
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads all diseases" do
+      sign_in patient
+      get :index
+      expect(assigns(:diseases)).to match_array([disease_1, disease_2])
     end
   end
 
@@ -18,6 +33,12 @@ RSpec.describe DiseasesController, type: :controller do
       sign_in patient
       get :show, { id: disease.id}
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      sign_in patient
+      get :show, { id: disease.id}
+      expect(response).to render_template("show")
     end
   end
 end
