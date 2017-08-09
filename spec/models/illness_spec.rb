d49456f1734f75ceb635e5da2345315dcb2c155d
Anton <anton@ondoc.me>
2017-08-09 23:44:25 +0300

    Write test to Illness model

--- spec/models/illness_spec.rb
+++ spec/models/illness_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Illness, type: :model do
   it { is_expected.to have_many(:visits) }
   it { is_expected.to have_many(:prescriptions) }
   it { is_expected.to have_many(:diagnoses) }
+
+  let!(:clinic_1) { create(:clinic) }
+  let!(:clinic_2) { create(:clinic) }
+  let!(:illness) do
+    create(:illness,
+           visits: [
+               create(:visit, clinic: clinic_2),
+               create(:visit, clinic: clinic_1)
+           ])
+  end
+
+  it 'метод clinic возвращает клинку из посленего приема по текущей болезни' do
+    expect(illness.clinic).to be == clinic_1
+  end
 end
