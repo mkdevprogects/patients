f8544fdeb621b07d344512534316161e50e46bb1
Stanislav Mekhonoshin <ejabberd@gmail.com>
2017-08-12 12:38:36 +0300

    Refactor HasHumanDate specs

--- spec/decorators/has_human_date_spec.rb
+++ spec/decorators/has_human_date_spec.rb
@@ -1,28 +1,32 @@
 require 'spec_helper'
 
 describe HasHumanDate do
-  include HasHumanDate
-  subject { IllnessDecorator.new(illness) }
-  let(:illness) { create(:illness, symptoms: [create(:symptom), create(:symptom)], diseases: [create(:disease)]) }
-  let!(:symptom) { SymptomDecorator.new(illness.symptoms.first) }
-  let!(:visit_1) { create(:visit, illness_id: illness.id) }
-  let!(:visit) { VisitDecorator.new(visit_1) }
-  let!(:visit_2) { create(:visit, illness_id: illness.id) }
-  let!(:prescription_1) { create(:prescription, illness_id: illness.id) }
+  describe '#human_created_at' do
+    subject { IllnessDecorator.new(illness) }
+    let(:illness) { create(:illness) }
 
-  it 'date should return date on format "day - month - year"' do
-    expect(date(subject.created_at)).to eq illness.created_at.strftime("%d - %m - %Y")
+    it 'human_created_at should return string, where date eq obj.created_at' do
+      expect(subject.human_created_at).to eq illness.created_at.strftime("%d - %m - %Y")
+    end
   end
 
-  it 'human_created_at should return string, where date eq obj.created_at' do
-    expect(subject.human_created_at).to eq date(illness.created_at)
+  describe '#human_date_time' do
+    subject { VisitDecorator.new(visit) }
+    let(:visit) { create(:visit) }
+
+    it 'human_date_time should return date on format "day - month - year"' do
+      expect(subject.human_date_time).to eq visit.date_time.strftime("%d - %m - %Y")
+    end
   end
 
-  it 'human_date_time should return date on format "day - month - year"' do
-    expect(visit.human_date_time).to be == date(visit_1.date_time)
-  end
+  describe '#human_first_occurence_date' do
+    subject { SymptomDecorator.new(symptom) }
+    let(:symptom) { create(:symptom) }
 
-  it 'human_first_occurence_date should return date on format "day - month - year"' do
-    expect(symptom.human_first_occurence_date).to be == date(symptom.first_occurence_date)
+    before { create(:illness_symptom, symptom: symptom) }
+
+    it 'human_first_occurence_date should return date on format "day - month - year"' do
+      expect(subject.human_first_occurence_date).to eq symptom.first_occurence_date.strftime("%d - %m - %Y")
+    end
   end
 end
