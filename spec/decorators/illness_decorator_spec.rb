7b473330c69171d5f2a78f9269149ff32bbc5805
Stanislav Mekhonoshin <ejabberd@gmail.com>
2017-08-12 11:59:25 +0300

    Spec improves

--- spec/decorators/illness_decorator_spec.rb
+++ spec/decorators/illness_decorator_spec.rb
@@ -3,11 +3,10 @@ require 'spec_helper'
 describe IllnessDecorator do
   subject { described_class.new(illness) }
   let(:illness) { create(:illness, symptoms: [create(:symptom), create(:symptom)], diseases: [create(:disease)]) }
-  let!(:visit_1) { create(:visit, illness_id: illness.id) }
-  let!(:visit_2) { create(:visit, illness_id: illness.id) }
-  let!(:prescription_1) { create(:prescription, illness_id: illness.id) }
+  let!(:visit_1) { create(:visit, illness: illness) }
+  let!(:visit_2) { create(:visit, illness: illness) }
+  let!(:prescription_1) { create(:prescription, illness: illness) }
 
-  # Совершенно не уверен, что это надо делать так
   it 'visits should return decorate object' do
     expect(subject.visits).to be_decorated
   end
@@ -21,7 +20,7 @@ describe IllnessDecorator do
   end
 
   it 'doctors_surname_name should return string, contains doctor surname and doctor first name' do
-    expect(subject.doctors_surname_name).to be == "#{illness.doctor.surname} #{illness.doctor.name}"
+    expect(subject.doctors_surname_name).to eq "#{illness.doctor.surname} #{illness.doctor.name}"
   end
 
   it 'first_disease_title should return title' do
