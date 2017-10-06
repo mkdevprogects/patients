945b22a60bbd3bc6d94f9a9372513c4f73e739bd
Anton <anton@ondoc.me>
2017-10-07 01:57:11 +0300

    Create IllnessRequest show

--- app/controllers/illness_requests_controller.rb
+++ app/controllers/illness_requests_controller.rb
@@ -1,4 +1,13 @@
 class IllnessRequestsController < ApplicationController
+
+  def index
+    @illness_requests = current_patient.illness_requests
+  end
+
+  def show
+    @illness_request = IllnessRequest.find(params[:id])
+  end
+
   def new
     @symptoms = Symptom.all
     @illness_request = IllnessRequest.new
@@ -16,6 +25,12 @@ class IllnessRequestsController < ApplicationController
   private
 
   def illness_request_params
-    params.require(:illness_request).permit(:admissible_cost, :allowable_reception_time, :distance, symptom_ids: [])
+    params.require(:illness_request).permit(
+        :admissible_cost,
+        :allowable_reception_time,
+        :distance,
+        :patient_id,
+        symptom_ids: []
+    )
   end
 end
