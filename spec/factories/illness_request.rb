91997b401e24ee93398b41a77bb9f663149bb8df
Anton <anton@ondoc.me>
2017-10-08 00:13:58 +0300

    Create tests to IllnessRequestsController

--- spec/factories/illness_request.rb
+++ spec/factories/illness_request.rb
@@ -1,9 +1,10 @@
 FactoryGirl.define do
-  factory :illness_requests do
+  factory :illness_request do
     patient
     illness
     sequence(:distance) { rand(1..30) }
-    sequence(:admissible_cost) { (rand(1..100)..rand(101..1000)) }
-    sequence(:allowable_reception_time) { (Time.now..(Time.now + 7200)) }
+    # sequence(:admissible_cost) { (rand(1..100)..rand(101..1000)) }
+    # sequence(:allowable_reception_time) { (Time.now..(Time.now + 7200)) }
+    after(:build) { |illness_request| illness_request.symptoms << create(:symptom) }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,3 +21,7 @@ end
 def email
   "qwerty#{rand(1000)}@gmail.com"
 end
+
+def patient_sign_in
+  redirect_to('/patient/sign_in')
+end
