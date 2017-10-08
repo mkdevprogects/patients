5fe32cea41c25f0f98cc6779bdbab6aa2e129486
Anton <anton@ondoc.me>
2017-10-09 00:18:08 +0300

    Remove the commented code

--- spec/factories/illness_request.rb
+++ spec/factories/illness_request.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
     patient
     illness
     sequence(:distance) { rand(1..30) }
-    # sequence(:admissible_cost) { (rand(1..100)..rand(101..1000)) }
-    # sequence(:allowable_reception_time) { (Time.now..(Time.now + 7200)) }
     after(:build) { |illness_request| illness_request.symptoms << create(:symptom) }
   end
 end
