b5fd49010ad3643d820120f459737edce06a3302
Anton <anton@ondoc.me>
2017-10-18 08:24:39 +0300

    Create Services::IllnessRequestCreator

---
+++ services/illness_request_creator.rb
@@ -0,0 +1,10 @@
+class Services
+  class IllnessRequestCreator
+
+    def initialize(illness_request_id)
+      Hutch.connect
+      Hutch.publish('illness.request.new', subject: illness_request_id)
+    end
+  end
+end
+
