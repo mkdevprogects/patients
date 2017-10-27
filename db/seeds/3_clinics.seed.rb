e7a7cb710475bf8d42d30ac4223970c54badc924
Anton <anton@ondoc.me>
2017-10-28 00:23:51 +0300

    Update seeds

--- db/seeds/3_clinics.seed.rb
+++ db/seeds/3_clinics.seed.rb
@@ -1,11 +1,60 @@
 clinics = [
-    {title: 'Медлайн', phone: '1111111111', address: 'Невский просп., д. 2', email: 'clinic.1.@clinic.com'},
-    {title: 'АЦЗ', phone: '2222222222', address: 'ул. Широкая, д. 15', email: 'clinic.2.@clinic.com'},
-    {title: 'ЕМС', phone: '3333333333', address: 'ул. Щепкина, д. 25', email: 'clinic.3.@clinic.com'},
-    {title: 'Полимедика', phone: '444444444444', address: 'просп.Славы, д. 100', email: 'clinic.4.@clinic.com'},
-    {title: 'Аллегро', phone: '555555555555', address: 'наб. Калнала Грибоедова, д 25', email: 'clinic.5.@clinic.com'},
-    {title: 'МСЧ 125', phone: '555555555555', address: 'площадь Ленина, д. 2', email: 'clinic.6.@clinic.com'},
-    {title: 'РАМИ', phone: '4444444444', address: 'Лиговский просп., д. 10', email: 'clinic.7.@clinic.com'},
+    {
+        title: 'Медлайн',
+        phone: '1111111111',
+        address: 'Невский просп., д. 2, Санкт-Петербург',
+        email: 'clinic.1.@clinic.com',
+        latitude: 59.937197,
+        longitude: 30.312927
+    },
+    {
+        title: 'АЦЗ',
+        phone: '2222222222',
+        address: 'ул. Широкая, д. 15, Москва',
+        email: 'clinic.2.@clinic.com',
+        latitude: 55.83737410000001,
+        longitude: 37.2530661
+    },
+    {
+        title: 'ЕМС',
+        phone: '3333333333',
+        address: 'ул. Щепкина, д. 25б Москва',
+        email: 'clinic.3.@clinic.com',
+        latitude: 55.7778263,
+        longitude: 37.6287344
+    },
+    {
+        title: 'Полимедика',
+        phone: '444444444444',
+        address: 'просп.Славы, д. 100, Белгород',
+        email: 'clinic.4.@clinic.com',
+        latitude: 50.5998019,
+        longitude: 36.575649
+    },
+    {
+        title: 'Аллегро',
+        phone: '555555555555',
+        address: 'наб. Калнала Грибоедова, д 25, Санкт-Петербург',
+        email: 'clinic.5.@clinic.com',
+        latitude: 59.93292650000001,
+        longitude: 30.3249209
+    },
+    {
+        title: 'МСЧ 125',
+        phone: '555555555555',
+        address: 'площадь Ленина, д. 2, Санкт-Петербург',
+        email: 'clinic.6.@clinic.com',
+        latitude: 59.95462,
+        longitude: 30.354058
+    },
+    {
+        title: 'РАМИ',
+        phone: '4444444444',
+        address: 'Лиговский просп., д. 10, Санкт-Петербург',
+        email: 'clinic.7.@clinic.com',
+        latitude: 59.93208949999999,
+        longitude: 30.3621571
+    },
 ]
 
 clinics.each do |params|
--- db/seeds/7_patients.seed.rb
+++ db/seeds/7_patients.seed.rb
@@ -1,14 +1,37 @@
 patients = [
-    {name: 'Илья', surname: 'Гордиенко', patronymic: 'Арсеньевич',
-     phone: '11111111111', email: 'patient.1.@patient.com', password: '123123'},
-    {name: 'Кирилл', surname: 'Семкин', patronymic: 'Сергеевич',
-     phone: '11111111112', email: 'patient.2.@patient.com', password: '123123'},
-    {name: 'Олег', surname: 'Осташков', patronymic: 'Олегович',
-     phone: '11111111113', email: 'patient.3.@patient.com', password: '123123'},
-    {name: 'Константин', surname: 'Жданов', patronymic: 'Александрович',
-     phone: '11111111114', email: 'patient.4.@patient.com', password: '123123'},
-    {name: 'Роман', surname: 'Лапин', patronymic: 'Евгеньевич',
-     phone: '11111111115', email: 'patient.5.@patient.com', password: '123123'}
+    {
+        name: 'Илья',
+        surname: 'Гордиенко',
+        patronymic: 'Арсеньевич',
+        phone: '11111111111',
+        email: 'patient.1.@patient.com',
+        password: '123123',
+        address: 'Мичуринский просп., д. 7, Москва',
+        latitude: 55.7028203,
+        longitude: 37.5099813
+    },
+    {
+        name: 'Кирилл',
+        surname: 'Семкин',
+        patronymic: 'Сергеевич',
+        phone: '11111111112',
+        email: 'patient.2.@patient.com',
+        password: '123123',
+        address: 'Лесной просп., д. 10, Санкт-Петербург',
+        latitude: 59.96471440000001,
+        longitude: 30.3511009
+    },
+    {
+        name: 'Олег',
+        surname: 'Осташков',
+        patronymic: 'Олегович',
+        phone: '11111111113',
+        email: 'patient.3.@patient.com',
+        password: '123123',
+        address: 'ул. Федора Абрамова, д. 20, Санкт-Петербург',
+        latitude: 60.0813446,
+        longitude: 30.3408908
+    }
 ]
 
 patients.each do |params|
